9eb85888c441e9a5dfa269a439aadf478a6fe0d8
Joakim Bech <joakim.bech@linaro.org>
2019-02-28 14:49:34 +0100

    Move OP-TEE specific TPM2.0 ref files to folders correspond to the ref implementation
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- Samples/ARM32-FirmwareTPM/optee_ta/fTPM/sub.mk
+++ Samples/ARM32-FirmwareTPM/optee_ta/fTPM/sub.mk
@@ -34,20 +34,29 @@ subdirs-y += lib
 global-incdirs-y += include
 global-incdirs-y += reference/include
 global-incdirs-y += op-tee/include
+global-incdirs-y += op-tee/TPMCmd/Platform/include/
+global-incdirs-y += op-tee/TPMCmd/Platform/include/prototypes/
 
+# Files that comes from the MS 2.0 reference implementation but has been
+# modified to work with OP-TEE. Note that we have removed the original files
+# that otherwise would have been found under the same folder, but under
+# 'lib/TPMCmd/... instead.
+srcs-y += op-tee/TPMCmd/Platform/src/Cancel.c
+srcs-y += op-tee/TPMCmd/Platform/src/Clock.c
+srcs-y += op-tee/TPMCmd/Platform/src/Entropy.c
+srcs-y += op-tee/TPMCmd/Platform/src/LocalityPlat.c
+srcs-y += op-tee/TPMCmd/Platform/src/NVMem.c
+srcs-y += op-tee/TPMCmd/Platform/src/PlatformData.c
+srcs-y += op-tee/TPMCmd/Platform/src/PowerPlat.c
+srcs-y += op-tee/TPMCmd/Platform/src/PPPlat.c
+srcs-y += op-tee/TPMCmd/Platform/src/RunCommand.c
+srcs-y += op-tee/TPMCmd/Platform/src/Unique.c
+
+# Files that are unique to the OP-TEE implementation.
 srcs-y += op-tee/AdminPPI.c
-srcs-y += op-tee/Cancel.c
-srcs-y += op-tee/Clock.c
-srcs-y += op-tee/Entropy.c
-srcs-y += op-tee/LocalityPlat.c
-srcs-y += op-tee/NvAdmin.c
-srcs-y += op-tee/NVMem.c
-srcs-y += op-tee/PowerPlat.c
-srcs-y += op-tee/PlatformData.c
-srcs-y += op-tee/PPPlat.c
-srcs-y += op-tee/RunCommand.c
-srcs-y += op-tee/Unique.c
 srcs-y += op-tee/EPS.c
+srcs-y += op-tee/NvAdmin.c
+
 srcs-y += reference/RuntimeSupport.c
 
 srcs-y += fTPM.c
